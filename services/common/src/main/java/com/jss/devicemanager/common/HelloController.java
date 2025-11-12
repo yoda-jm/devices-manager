557007b9cb342eb28e52c11371444dcf615a1a24
Vincent Le Ligeour <yoda-jm@users.sourceforge.net>
2025-11-12 23:55:22 +0100

    feat: use spring.application.name in hello endpoint

--- services/common/src/main/java/com/jss/devicemanager/common/HelloController.java
+++ services/common/src/main/java/com/jss/devicemanager/common/HelloController.java
@@ -2,6 +2,7 @@ package com.jss.devicemanager.common;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,12 @@ public class HelloController {
 
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @GetMapping("/")
     public String hello() {
         logger.debug("Hello endpoint called");
-        return "Hello World!";
+        return "Hello from " + applicationName + "!";
     }
 }
